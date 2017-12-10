bf538207425f4f15c034179f336dc24d5aa19c41
Kezhu Wang <kezhuw@gmail.com>
2017-12-10 14:20:35 +0800

    Define thunk as a function that accepts a store and returns a value

--- lib/src/thunk.dart
+++ lib/src/thunk.dart
@@ -4,10 +4,19 @@ import 'package:meta/meta.dart';
 
 import 'store.dart';
 
-typedef Future<R> Thunk<S, R>(Store<S> store);
+/// Thunk is a function that accepts a store and returns a value.
+typedef R Thunk<S, R>(Store<S> store);
 
+/// VoidThunk is a special [Thunk] that returns nothing.
+typedef void VoidThunk<S> (Store<S> store);
+
+/// AsyncThunk is a special [Thunk] that returns a [Future].
+typedef Future<R> AsyncThunk<S, R>(Store<S> store);
+
+/// ThunkAction is an action type for [Thunk]. It restrict thunk to store with
+/// specified state type [S], and store the result in field [result].
 @immutable
-class ThunkAction<S, R> extends RequestAction<Future<R>> {
+class ThunkAction<S, R> extends RequestAction<R> {
   ThunkAction(this.thunk);
 
   final Thunk<S, R> thunk;
@@ -20,8 +29,44 @@ class ThunkAction<S, R> extends RequestAction<Future<R>> {
   }
 }
 
-void _handleThunkAction(Store<dynamic> store, ThunkAction<dynamic, dynamic> action, Dispatcher next) {
-  action(store);
+/// VoidThunkAction is an action type for [VoidThunk]. It restrict thunk to store
+/// with specified state type [S].
+///
+/// Due to type system limitation, it actually extends from ThunkAction<S, Null>,
+/// not ThunkAction<S, void>. The [result] value is always null.
+///
+/// See: https://github.com/dart-lang/sdk/issues/28943 for void as generic type
+/// argument.
+@immutable
+class VoidThunkAction<S> extends ThunkAction<S, Null> {
+  VoidThunkAction(VoidThunk<S> thunk) : super((Store<S> store) {
+    thunk(store);
+    return null;
+  });
 }
 
-Middleware thunkMiddleware = new ProxyTypedMiddleware<dynamic, ThunkAction>(_handleThunkAction);
+/// AsyncThunkAction is an action type for [AsyncThunk]. It restrict thunk to
+/// store with specified state type [S]. The [result] is a [Future] with [R] as
+/// its type argument.
+@immutable
+class AsyncThunkAction<S, R> extends ThunkAction<S, Future<R>> {
+  AsyncThunkAction(AsyncThunk<S, R> thunk) : super(thunk);
+}
+
+/// A middleware that intercepts [Function] like actions, call that function
+/// instead of passing control to next [Dispatcher].
+///
+/// See [ThunkAction], [VoidThunkAction] and [AsyncThunkAction] for predefined
+/// thunk action types, clients are free to create custom thunk action types as
+/// long as those types are [Function] like.
+///
+/// Client can restrict this middleware to more concrete state type by wrap it
+/// in [ProxyTypedMiddleware].
+void thunkMiddleware(Store<dynamic> store, Action action, Dispatcher next) {
+  if (action is Function) {
+    Function f = action as Function;
+    f(store);
+    return;
+  }
+  next(action);
+}
--- test/thunk_test.dart
+++ test/thunk_test.dart
@@ -11,45 +11,109 @@ class BarState {
 
 void main() {
   group('Thunk actions', () {
-    Store<FooState> store = new Store<FooState>(
-      name: 'foo',
-      initialState: new FooState(),
-      reducer: null,
-      middlewares: [thunkMiddleware],
-    );
+    List<String> logs = [];
+    Store<FooState> fooStore;
+    Store<BarState> barStore;
 
-    Future<String> thunk(Store<FooState> store) async {
+    setUp(() {
+      logs.clear();
+
+      fooStore = new Store<FooState>(
+        name: 'foo',
+        initialState: new FooState(),
+        reducer: null,
+        middlewares: [thunkMiddleware],
+      );
+
+      barStore = new Store<BarState>(
+        name: 'foo',
+        initialState: new BarState(),
+        reducer: null,
+        middlewares: [thunkMiddleware],
+      );
+    });
+
+    void voidFooThunk(Store<FooState> store) {
+      logs.add('void foo thunk');
+    }
+
+    String syncFooThunk(Store<FooState> store) {
+      logs.add('sync foo thunk');
+      return 'syncFooThunk';
+    }
+
+    Future<String> asyncFooThunk(Store<FooState> store) async {
+      logs.add('async foo thunk');
       return await new Future<String>.value('foo thunk result');
     }
 
-    test('thunk action is called for matching state', () async {
-      ThunkAction<FooState, String> action = new ThunkAction<FooState, String>(thunk);
+    test('thunk action is called for matching state', () {
+      ThunkAction<FooState, String> action = new ThunkAction<FooState, String>(syncFooThunk);
+
+      fooStore.dispatch(action);
+
+      expect(logs, ['sync foo thunk']);
+      expect(action.result, 'syncFooThunk');
+    });
+
+    test('thunk action is skipped for mismatched state', () {
+      ThunkAction<FooState, String> action = new ThunkAction<FooState, String>(syncFooThunk);
+
+      barStore.dispatch(action);
+
+      expect(logs, []);
+      expect(action.result, isNull);
+    });
+
+    test('void thunk action is called for matching state', () {
+      VoidThunkAction<FooState> action = new VoidThunkAction<FooState>(voidFooThunk);
+
+      fooStore.dispatch(action);
+
+      expect(logs, ['void foo thunk']);
+      expect(action.result, isNull);
+    });
+
+    test('void thunk action is skipped for mismatched state', () {
+      ThunkAction<FooState, Null> action = new VoidThunkAction<FooState>(voidFooThunk);
+
+      barStore.dispatch(action);
+
+      expect(logs, []);
+      expect(action.result, isNull);
+    });
+
+    test('async thunk action is called for matching state', () async {
+      AsyncThunkAction<FooState, String> action = new AsyncThunkAction<FooState, String>(asyncFooThunk);
 
       expect(action.result, isNull);
 
-      store.dispatch(action);
+      fooStore.dispatch(action);
 
       expect(action.result, isNotNull);
-
       expect(action.result, const isInstanceOf<Future<String>>());
 
       String s = await action.result;
 
       expect(s, 'foo thunk result');
+
+      expect(logs, ['async foo thunk']);
     });
 
-    test('thunk action is skipped for mismatched state', () async {
+    test('async thunk action is skipped for mismatched state', () async {
       Future<String> barThunk(Store<BarState> store) async {
         return await new Future<String>.value('bar thunk result');
       }
 
-      ThunkAction<BarState, String> action = new ThunkAction<BarState, String>(barThunk);
+      ThunkAction<BarState, Future<String>> action = new AsyncThunkAction<BarState, String>(barThunk);
 
       expect(action.result, isNull);
 
-      store.dispatch(action);
+      fooStore.dispatch(action);
 
       expect(action.result, isNull);
+
+      expect(logs, []);
     });
   });
 }
